24afa457fd2c19fe72bd92bb62fb9a00f1aa8823
gui1612 <gui1612@sapo.pt>
2021-04-24 18:34:01 +0100

    feat: added the first arm assembly worksheet

---
+++ F3 - Basic AArch Instructions/biggerbetween.s
@@ -0,0 +1,9 @@
+.text
+.global biggerbetween
+.type biggerbetween, "function"
+
+biggerbetween:		MOV X9, X11				// Puts X11 in X9
+					CMP X10, X11
+					B.LT final
+					MOV X9, X10
+final: 				RET						// At this phase, X9 will contain the biggest value between X10 and X11
