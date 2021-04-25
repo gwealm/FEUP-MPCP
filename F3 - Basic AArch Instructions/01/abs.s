1143d5e0affe0424a0b2ae9a821f76768a3659dc
gui1612 <gui1612@sapo.pt>
2021-04-25 15:52:27 +0100

    beautify: fixed identation in exercise 1 (F3)

--- F3 - Basic AArch Instructions/01/abs.s
+++ F3 - Basic AArch Instructions/01/abs.s
@@ -2,5 +2,5 @@
 .global absx15;
 .type absx15, "function"
 
-absx15:			CNEG X15, X15, MI
-				RET
+absx15:		CNEG X15, X15, MI
+		RET
--- F3 - Basic AArch Instructions/01/biggerbetween.s
+++ F3 - Basic AArch Instructions/01/biggerbetween.s
@@ -2,8 +2,8 @@
 .global biggerbetween
 .type biggerbetween, "function"
 
-biggerbetween:		MOV X9, X11				// Puts X11 in X9
-					CMP X10, X11
-					B.LT final
-					MOV X9, X10
-final: 				RET						// At this phase, X9 will contain the biggest value between X10 and X11
+biggerbetween:		MOV X9, X11			// Puts X11 in X9
+			CMP X10, X11
+			B.LT final
+			MOV X9, X10
+final: 			RET				// At this phase, X9 will contain the biggest value between X10 and X11
