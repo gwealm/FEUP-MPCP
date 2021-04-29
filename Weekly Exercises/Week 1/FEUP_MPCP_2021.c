a68303bfe310070a1dbd00ae81445d57064b9025
gui1612 <gui1612@sapo.pt>
2021-04-30 00:06:28 +0100

    feat: added weekly assignments

---
+++ Weekly Exercises/Week 1/FEUP_MPCP_2021.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int ContPar(long *array, long size);
+
+int main(void) {
+
+	/*		
+		    long size = 0;
+            long array[] = {};
+            long evens = ContPar(array, size);
+            printf("Resultado: %ld", evens);
+	*/
+
+	return EXIT_SUCCESS;
+}
